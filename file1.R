9aa34eed31278445d8eea8398525965f93eceaef
sidkota <sid.kota123@gmail.com>
2018-10-23 23:11:21 +0530

    update and push

--- file1.R
+++ file1.R
@@ -1,3 +1,25 @@
-mtcras
+women #ctrl+ enter to run
 mtcars
-women
+
+?women #Getting help on data set
+
+data() #all data sets
+AirPassengers
+
+plot(women) #plot a simple graph
+
+plot(women, type='b', lwd=3) #graph customisation
+
+plot(women, xlab = "Height (in)", ylab = "Weight (lb)",
+     main = "women data: American women aged 30-39")  #example from help
+
+x=c(1, 3, 5) #vector, ?c to know what is c
+x1=1:10 #vector/list
+x
+x1
+
+
+
+attach(mtcars)
+mpg
+mtcars
